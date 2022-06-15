2a9d60144e1876a0bdb05ef5e8d032e634288d53
Márcio Henrique <marcioh.ikke@gmail.com>
2022-06-15 13:56:52 -0300

    update MainDashboard

--- src/components/MainDashboard.vue
+++ src/components/MainDashboard.vue
@@ -1,9 +1,24 @@
 <template>
   <v-container>
-    <v-row
-        :align="center"
-        no-gutters
-      >
+    <v-row no-gutters>
+      <v-col v-for="n in 12" :key="n" cols="12" sm="3">
+        <!-- <v-card class="pa-2" outlined tile> One of three columns </v-card> -->
+        <v-select
+          :label="labels[n-1]"
+          v-model="selected"
+          :items="select"
+          item-text="value"
+          outlined
+          multiple
+          return-object
+          style="padding-right: 20px"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <!-- <div class="center">
+    <p>The text within the div is centered horizontally.</p>
+    </div> -->
+    <v-row :align="center" no-gutters>
       <v-col cols="5" v-for="(chartConfig, i) in chartConfigs" :key="i + 1">
         <chart-card class="pa-2" :chartConfig="chartConfig" :filters="filters">
         </chart-card>
@@ -50,6 +65,32 @@ export default {
           count: "id",
           title: "Extensão do Arquivo",
         },
+      ],
+      select: [
+        {
+          value: "Impresso",
+        },
+        {
+          value: "Digital",
+        },
+        {
+          value: "Audiovisual",
+        },
+      ],
+      selected: [],
+      labels: [
+        "Edital",
+        "Editora",
+        "Coleção",
+        "Volume",
+        "Série",
+        "Componente",
+        "Exemplar",
+        "Formato",
+        "Tipo do Recurso",
+        "Tipo de Arquivo",
+        "Nome do Arquivo",
+        "Extensão do Arquivo",
       ],
     };
   },
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,32 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import pt from 'vuetify/lib/locale/pt';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const opts = {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      options: {
+        customProperties: true,
+      },
+      themes: {
+        light: {
+          primary: '#0098DA',
+          green: '#6AD90B',
+          red: '#D91636',
+          yellow: '#D9D016',
+          background: '#f4f5f7',
+          'snow-white': '#F8F9FA',
+          'cloudy-grey': '#353B42',
+          'muddy-grey': '#666666',
+        },
+      },
+    },
+    lang: {
+      locales: { pt },
+      current: 'pt',
+    },
+  };
+
+export default new Vuetify(opts);
